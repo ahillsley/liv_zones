002eee67a778f70010c4f1d68d29f25ae8ac8c87
Alex Hillsley <avhillsley@utexas.edu>
2023-12-11 12:47:12 -0500

    Add vein coords for all acini

--- liv_zones/veincoords.py
+++ liv_zones/veincoords.py
@@ -2,70 +2,281 @@
 
 import os, pickle
 
-os.chdir('//prfs.hhmi.org/felicianolab/For_Alex_and_Mark/Male/CNT/Liv1/Lobule1')
-CV_coords = [7630,10532]
-PV_coords =[[5537,2410],[13948,3961],[3017,18517]]
-with open('vein_coords.pickle','wb') as f:
-    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
-    
-os.chdir('//prfs.hhmi.org/felicianolab/For_Alex_and_Mark/Male/CNT/Liv1/Lobule2')
-CV_coords = [9523,6929]
-PV_coords =[[2511,2176],[7193,15114],[19092,10861]]
-with open('vein_coords.pickle','wb') as f:
-    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
-    
-os.chdir('//prfs.hhmi.org/felicianolab/For_Alex_and_Mark/Male/CNT/Liv2/Lobule1')
-CV_coords = [10820,10644]
-PV_coords =[[15061,2268],[19991,6501],[2939,14323]]
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/STV/Liv1/Lobule3')
+CV_coords = [6048, 11928]
+PV_coords =[[9959, 3431],[2999, 18647],[9863,18935]]
 with open('vein_coords.pickle','wb') as f:
     pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
 
-os.chdir('//prfs.hhmi.org/felicianolab/For_Alex_and_Mark/Male/CNT/Liv2/Lobule2')
-CV_coords = [10463,3403]
-PV_coords =[[2273,2864],[5770,9985],[17981,5667]]
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/STV/Liv2/Lobule1')
+CV_coords = [13584, 5744]
+PV_coords =[[2927, 2367],[7615, 10783],[22015,8847]]
 with open('vein_coords.pickle','wb') as f:
     pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
 
-os.chdir('//prfs.hhmi.org/felicianolab/For_Alex_and_Mark/Male/CNT/Liv2/Lobule3')
-CV_coords = [9043,2683]
-PV_coords =[[2613,9073],[14240,9547],[18447,2791]]
-with open('vein_coords.pickle','wb') as f:
-    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
-    
-os.chdir('//prfs.hhmi.org/felicianolab/For_Alex_and_Mark/Male/CNT/Liv3/Lobule1')
-CV_coords = [14906,9117]
-PV_coords =[[4513,5404],[15840,2413],[18744,15415]]
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/STV/Liv2/Lobule2')
+CV_coords = [12312, 11424]
+PV_coords =[[13535, 3119],[3263, 8615],[13295, 19511]]
 with open('vein_coords.pickle','wb') as f:
     pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
 
-os.chdir('//prfs.hhmi.org/felicianolab/For_Alex_and_Mark/Male/CNT/Liv3/Lobule2')
-CV_coords = [13500,8930]
-PV_coords =[[3814,4331],[4237,12304],[17468,16241]]
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/STV/Liv2/Lobule3')
+CV_coords = [2848, 8384]
+PV_coords =[[9183, 3647],[11423, 10383],[5791, 14399]]
 with open('vein_coords.pickle','wb') as f:
     pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
 
-os.chdir('//prfs.hhmi.org/felicianolab/For_Alex_and_Mark/Male/CNT/Liv3/Lobule3')
-CV_coords = [10679,12176]
-PV_coords =[[2055,11588],[13951,3022],[19665,15857]]
-with open('vein_coords.pickle','wb') as f:
-    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
-    
-os.chdir('//prfs.hhmi.org/felicianolab/For_Alex_and_Mark/Male/CNT/Liv5/Lobule1')
-CV_coords = [8956,10495]
-PV_coords =[[2169,4228],[13550,2587],[6644,17987]]
-with open('vein_coords.pickle','wb') as f:
-    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
-    
-os.chdir('//prfs.hhmi.org/felicianolab/For_Alex_and_Mark/Male/CNT/Liv5/Lobule2')
-CV_coords = [8564,13759]
-PV_coords =[[5112,3239],[20552,6879],[2759,23497]]
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/STV/Liv3/Lobule1')
+CV_coords = [11424, 9288]
+PV_coords =[[17087, 3383],[2879, 3575],[10799, 16247]]
 with open('vein_coords.pickle','wb') as f:
     pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
 
-os.chdir('//prfs.hhmi.org/felicianolab/For_Alex_and_Mark/Male/CNT/Liv5/Lobule3')
-CV_coords = [13124,5200]
-PV_coords =[[2070,3413],[10042,13481],[22398,6646]]
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/STV/Liv3/Lobule2')
+CV_coords = [7776, 7360]
+PV_coords =[[4191, 2063],[3615, 13775],[12799, 12223]]
 with open('vein_coords.pickle','wb') as f:
     pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
 
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/STV/Liv3/Lobule3')
+CV_coords = [4000, 7984]
+PV_coords =[[9599, 12175],[11375, 8015],[2623, 2335]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
 
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/STV/Liv5/Lobule1')
+CV_coords = [8568, 10080]
+PV_coords =[[2255, 4127],[7007, 16511],[13511, 5639]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/STV/Liv5/Lobule2')
+CV_coords = [9408, 11232]
+PV_coords =[[2351, 9903],[15535, 3967],[16799, 12735]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/STV/Liv5/Lobule3')
+CV_coords = [8880, 8672]
+PV_coords =[[14191, 2319],[16063, 8527],[2815, 12079]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/STV/Liv5/Lobule3')
+CV_coords = [8880, 8672]
+PV_coords =[[14191, 2319],[16063, 8527],[2815, 12079]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+# ------- Western Diet ----------
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/WD/Liv1/Lobule1')
+CV_coords = [12992, 3584]
+PV_coords =[[2623, 3679],[24063, 2975],[11551, 14943]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/WD/Liv1/Lobule2')
+CV_coords = [13184, 14880]
+PV_coords =[[10719, 2367],[24735, 12975],[2479, 15935]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/WD/Liv1/Lobule3')
+CV_coords = [12064, 11760]
+PV_coords =[[7839, 2623],[20559, 9487],[2911, 15151]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/WD/Liv2/Lobule1')
+CV_coords = [4560, 11520]
+PV_coords =[[2231, 2591],[8663, 4343],[3263, 21383]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/WD/Liv2/Lobule2')
+CV_coords = [8568, 8784]
+PV_coords =[[3191, 2087],[15287, 6263],[12839, 17855]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/WD/Liv2/Lobule3')
+CV_coords = [9864, 11256]
+PV_coords =[[12839, 2471],[2351, 16223],[9695, 21647]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/WD/Liv3/Lobule1')
+CV_coords = [9156, 12780]
+PV_coords =[[2927, 20564],[22516, 15332],[15700, 2663]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/WD/Liv3/Lobule2')
+CV_coords = [11488, 8192]
+PV_coords =[[3007, 3359],[20351, 6047],[7359, 17407]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Male/WD/Liv3/Lobule3')
+CV_coords = [5536, 6464]
+PV_coords =[[2527, 18367],[15583, 12799],[13535, 2111]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+# --------- Female CNT -------------
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/CNT/Liv2/Lobule1')
+CV_coords = [11088, 11448]
+PV_coords =[[2735, 3071],[17519, 3455],[5807, 18119]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/CNT/Liv2/Lobule2')
+CV_coords = [8928, 10728]
+PV_coords =[[13919, 2975],[2135, 6839],[4103, 18911]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/CNT/Liv2/Lobule3')
+CV_coords = [4608, 11688]
+PV_coords =[[2423, 2519],[2303, 20807],[14375, 10679]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/CNT/Liv3/Lobule1')
+CV_coords = [7168, 10512]
+PV_coords =[[6431, 2255],[14319, 10895],[3375, 16095]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/CNT/Liv3/Lobule2')
+CV_coords = [10416, 8916]
+PV_coords =[[14519, 2771],[1511, 9599],[20699, 12239]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/CNT/Liv3/Lobule3')
+CV_coords = [13152, 11592]
+PV_coords =[[8351, 3551],[3071, 8303],[14279, 19751]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+# --------- Female STV -----------
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/STV/Liv1/Lobule1')
+CV_coords = [10128, 10224]
+PV_coords =[[3263, 3407],[20375, 9839],[12935, 17255]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/STV/Liv1/Lobule2')
+CV_coords = [7824, 10968]
+PV_coords =[[2783, 2543],[16175, 11087],[10391, 19799]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/STV/Liv1/Lobule3')
+CV_coords = [12432, 7872]
+PV_coords =[[18119, 2327],[1847, 6743],[17087, 17807]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/STV/Liv2/Lobule1')
+CV_coords = [7692, 7752]
+PV_coords =[[11051, 2723],[1883, 5891],[14087, 8747]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/STV/Liv2/Lobule2')
+CV_coords = [10056, 5400]
+PV_coords =[[13835, 2051],[5615, 8855],[1895, 4931]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/STV/Liv2/Lobule3')
+CV_coords = [10848, 11496]
+PV_coords =[[9359, 2351],[2015, 16391],[16103, 17087]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/STV/Liv4/Lobule1')
+CV_coords = [7752, 9096]
+PV_coords =[[2831, 2687],[13847, 3959],[6983, 16295]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/STV/Liv4/Lobule2')
+CV_coords = [12816, 10104]
+PV_coords =[[14255, 2327],[4031, 14591],[10463, 19823]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/STV/Liv4/Lobule3')
+CV_coords = [4512, 10488]
+PV_coords =[[4463, 2735],[2687, 19127],[14351, 13727]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+# --------- Female WD ----------------
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/WD/Liv1/Lobule1')
+CV_coords = [12840, 10656]
+PV_coords =[[16463, 3719],[4775, 5903],[3095, 16991]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/WD/Liv1/Lobule2')
+CV_coords = [1087, 12816]
+PV_coords =[[7223, 2615],[1871, 18407],[9215, 22631]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/WD/Liv1/Lobule3')
+CV_coords = [13416, 10128]
+PV_coords =[[9887, 2807],[2855, 11231],[15887, 17231]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/WD/Liv2/Lobule1')
+CV_coords = [9312, 10968]
+PV_coords =[[2903, 3407],[15503, 6671],[9119, 21983]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/WD/Liv2/Lobule2')
+CV_coords = [9780, 10932]
+PV_coords =[[18659, 10871],[12911, 2123],[2843, 6743]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/WD/Liv2/Lobule2')
+CV_coords = [9780, 10932]
+PV_coords =[[18659, 10871],[12911, 2123],[2843, 6743]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/WD/Liv2/Lobule2')
+CV_coords = [12120, 10632]
+PV_coords =[[16127, 3263],[3095, 17231],[11879, 21767]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/WD/Liv2/Lobule3')
+CV_coords = [12240, 10656]
+PV_coords =[[16247, 3119],[3143, 17087],[11951, 21743]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/WD/Liv3/Lobule1')
+CV_coords = [8543, 8943]
+PV_coords =[[2768, 2832],[16431, 5551],[13631, 16543]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
+
+os.chdir('/groups/feliciano/felicianolab/For_Alex_and_Mark/Female/WD/Liv3/Lobule2')
+CV_coords = [10320, 3520]
+PV_coords =[[15823, 14799],[4095, 11615],[2847, 2415]]
+with open('vein_coords.pickle','wb') as f:
+    pickle.dump([CV_coords,PV_coords],f,pickle.HIGHEST_PROTOCOL)
